9074cf673ac62d37389e43ea1219196f384f38c3
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-06-12 20:54:02 +0100

    4-tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
-""" Take the code from wait_n and alter it into a new function task_wait_n.
-    The code is nearly identical to wait_n except task_wait_random is being
-    called. """
+
+"""tasks"""
+
 import asyncio
+import random
 from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Tasks """
-    delays: List[float] = []
-    all_delays: List[float] = []
-    for i in range(n):
-        delays.append(task_wait_random(max_delay))
-    for delay in asyncio.as_completed(delays):
-        earliest_result = await delay
-        all_delays.append(earliest_result)
-    return all_delays
+    """
+    Test file for printing
+    the correct output of
+    the task_wait_n coroutine"""
+    f = [
+        task_wait_random(max_delay) for i in range(n)
+    ]
+
+    rs = []
+    for x in asyncio.as_completed(f):
+        r = await x
+        rs.append(r)
+    return rs
