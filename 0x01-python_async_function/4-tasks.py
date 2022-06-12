d01db4306351e775eb46041934e65156daf42bef
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-06-12 20:48:13 +0100

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,10 +10,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Test file for printing
-    the correct output of
-    the task_wait_n coroutine"""
+    """tasks"""
     f = [
         task_wait_random(max_delay) for i in range(n)
     ]
