1cf7d5d01fe15b95409695506ac6b672580b5c59
root <root@Mouadh.localdomain>
2022-06-13 14:54:15 +0100

    3. LRU Caching

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""caching system"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LIFOCache class"""
+
+    def __init__(self):
+        """constractor"""
+        super().__init__()
+        self.lru_order = []
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data
+        the item value for the key key"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.lru_order.remove(key)
+            else:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    del self.cache_data[self.lru_order[0]]
+                    print("DISCARD:", self.lru_order[0])
+                    self.lru_order.pop(0)
+                self.cache_data[key] = item
+            self.lru_order.append(key)
+
+    def get(self, key):
+        """return the value of key self.cache_data"""
+        if key in self.cache_data:
+            self.lru_order.remove(key)
+            self.lru_order.append(key)
+            return self.cache_data[key]
+        return None
