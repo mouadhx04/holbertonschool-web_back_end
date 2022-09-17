f4aff8e486647239d226da9c39eb097d8e88e22e
Mouadh Amemri <mouadh1500@gmail.com>
2022-09-17 18:10:33 +0100

    Basic list of objects

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 export default function getListStudents() {
-    return [
+  return [
       { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
       { id: 2, firstName: 'James', location: 'Columbia' },
       { id: 5, firstName: 'Serena', location: 'San Francisco' },
