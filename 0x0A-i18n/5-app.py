153b3426e54e029aa5dddd98a52821177b174551
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-09-19 11:40:18 +0100

    Mock logging in

---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+'''
+flask application
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from flask import g
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    ''' Config Class'''
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@app.route('/')
+def index():
+    '''0-index.html.'''
+    return render_template("5-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    '''determine the best match with our supported languages.'''
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    '''Returns user dictor None'''
+    try:
+        log_as = request.args.get('login_as')
+        return users[int(log_as)]
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    '''set a user as a global on flask.g.user. '''
+    g.user = get_user()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
