9108d2b434b3e0ee610f1052ad8cafcdc30a0df2
mouadhx04 <85496711+mouadhx04@users.noreply.github.com>
2022-09-19 16:50:54 +0100

    Force locale with URL parameter

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
-""" a basic flask app"""
+'''
+flask application
+'''
+
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 app = Flask(__name__)
-
-
-class Config(object):
-    """ Config class for Babel object """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
 babel = Babel(app)
 
 
+class Config(object):
+    ''' Config Class'''
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
 @app.route('/')
-def hello():
-    """ render a basic html file """
-    return render_template('4-index.html')
+def index():
+    '''0-index.html.'''
+    return render_template("4-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """ a function to determine the best match with the supported languages """
-    if request.full_path.split('/')[1][:8] == "?locale=":
-        lg = request.full_path.split('/')[1][8:]
-        if lg in app.config['LANGUAGES']:
-            return lg
+    '''determine the best match with our supported languages.'''
+    locale = request.args.get("locale")
+    if locale:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
