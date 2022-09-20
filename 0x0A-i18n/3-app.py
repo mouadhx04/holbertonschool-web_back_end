25fbaffacb0a3112bf146dc07f2c774d1354b5e0
Mouadh Amemri <mouadh1500@gmail.com>
2022-09-20 15:32:10 +0100

    Parametrize templates

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
-"""setup a basic Flask app"""
+'''
+flask application
+'''
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
-
-
-class Config:
-    """language configuration"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object("3-app.Config")
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """language best match"""
-    return request.accept_languages.best_match(
-       Config.LANGUAGES)
+class Config(object):
+    ''' Config Class'''
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
 
 
 @app.route('/')
 def index():
-    """render html template"""
-    return render_template('3-index.html')
+    '''0-index.html.'''
+    return render_template("2-index.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@babel.localeselector
+def get_locale():
+    '''determine the best match with our supported languages.'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
