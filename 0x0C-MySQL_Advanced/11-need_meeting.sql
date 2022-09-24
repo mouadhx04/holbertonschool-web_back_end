a8e359372341f943c8730551dd790254eea32665
Mouadh Amemri <mouadh1500@gmail.com>
2022-09-24 03:02:08 +0100

    No table for a meeting

---
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- creates a view need_meeting that lists all students
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting
+AS SELECT name FROM students
+WHERE score < 80 AND last_meeting is NULL
+OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH);
