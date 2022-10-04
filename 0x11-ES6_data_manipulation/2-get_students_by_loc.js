ab9b1430aa601a86d3ec6d26b4fbdd4a19508020
Mouadh Amemri <mouadh1500@gmail.com>
2022-10-04 15:13:34 +0100

    Filter by location

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(array, city) {
-  return array.filter((i) => i.location === city)
+export default function getStudentsByLocation (array, city) {
+  return array.filter((i) => i.location === city);
 }
