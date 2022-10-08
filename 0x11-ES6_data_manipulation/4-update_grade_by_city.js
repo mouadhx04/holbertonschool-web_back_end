359002c56521128f3797a89995f5e541ce407616
Mouadh Amemri <mouadh1500@gmail.com>
2022-10-08 18:27:25 +0100

    Combine by cita

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,15 @@
-export default function updateStudentGradeByCity(arrayofstudents, city, newgrades) {
-  return arrayofstudents.filter((i) => i.location === city).map((i) => {
-    const [newGrade] = newgrades.filter((item) => item.studentId === i.id);
-    return { ...i, grade: newGrade ? newGrade.grade : 'N/A' };
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  /* eslint-disable no-param-reassign */
+  if (Array.isArray(students) === false) {
+    return [];
+  }
+  return students.filter((x) => x.location === city).map((x) => {
+    x.grade = 'N/A';
+    for (const g of newGrades) {
+      if (g.studentId === x.id) {
+        x.grade = g.grade;
+      }
+    }
+    return x;
   });
 }
