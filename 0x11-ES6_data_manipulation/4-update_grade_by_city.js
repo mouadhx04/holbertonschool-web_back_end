58dc8ac3176a5660c60657bd8a9cca865238d4c9
Mouadh Amemri <mouadh1500@gmail.com>
2022-10-08 18:23:17 +0100

    Combine by city

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,33 +1,6 @@
-function updateStudentGradeByCity(students, city, newGrades) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-    if (!Array.isArray(newGrades)) {
-      return [];
-    }
-  
-    const stCity = students.filter((student) => student.location === city);
-  
-    const studentsGraded = stCity.map((student) => {
-      const gradeFilter = newGrades.filter(
-        (newGrade) => newGrade.studentId === student.id,
-      );
-  
-      let grade;
-  
-      if (gradeFilter[0]) {
-        grade = gradeFilter[0].grade;
-      } else {
-        grade = 'N/A';
-      }
-  
-      return {
-        ...student,
-        grade,
-      };
-    });
-  
-    return studentsGraded;
-  }
-  
-  export default updateStudentGradeByCity;
+export default function updateStudentGradeByCity(arrayofstudents, city, newgrades) {
+  return arrayofstudents.filter((i) => i.location === city).map((i) => {
+    const [newGrade] = newgrades.filter((item) => item.studentId === i.id);
+    return { ...i, grade: newGrade ? newGrade.grade : 'N/A' };
+  });
+}
