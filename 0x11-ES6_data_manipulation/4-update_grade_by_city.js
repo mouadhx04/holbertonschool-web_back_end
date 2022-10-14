897915ae9c19a9b1d2d19222af06324aa950c410
Mouadh Amemri <mouadh1500@gmail.com>
2022-10-14 03:31:18 +0100

    plz work

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,33 +1,7 @@
-function updateStudentGradeByCity(students, city, newGrades) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  if (!Array.isArray(newGrades)) {
-    return [];
-  }
-
-  const stCity = students.filter((student) => student.location === city);
-
-  const studentsGraded = stCity.map((student) => {
-    const gradeFilter = newGrades.filter(
-      (newGrade) => newGrade.studentId === student.id,
-    );
-
-    let grade;
-
-    if (gradeFilter[0]) {
-      grade = gradeFilter[0].grade;
-    } else {
-      grade = 'N/A';
-    }
-
-    return {
-      ...student,
-      grade,
-    };
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    const grades = newGrades.find((grade) => grade.studentId === student.id);
+    const grade = grades ? grades.grade : 'N/A';
+    return { ...student, grade };
   });
-
-  return studentsGraded;
 }
-
-export default updateStudentGradeByCity;
