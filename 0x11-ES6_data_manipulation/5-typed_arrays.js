b2fab26bbd15bbb4afbe8d43701fa494e65cb349
Mouadh Amemri <mouadh1500@gmail.com>
2022-10-14 03:40:54 +0100

    typed ! arrays

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,10 @@
-function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer, 0);
-
-  if (position > length - 1) {
+export default function createInt8TypedArray(length, position, value) {
+  try {
+    const buffer = new ArrayBuffer(length);
+    const dataView = new DataView(buffer);
+    dataView.setInt8(position, value);
+    return dataView;
+  } catch (e) {
     throw Error('Position outside range');
   }
-
-  view.setInt8(position, value);
-
-  return view;
 }
-
-export default createInt8TypedArray;
