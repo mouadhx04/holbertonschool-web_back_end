8c56b9df0f0e4c75f7933d8afda158975b3d023e
Mouadh Amemri <mouadh1500@gmail.com>
2022-10-14 04:02:17 +0100

    Clean set

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,8 @@
-export default function cleanSet (set,startString) {
-    if(startString?.length === 0 ) {
-      return "";
-    }
-    const arr = [];
-    set.forEach((element) => {
-      if (element?.startsWith(startString)) {
-        arr.push(element.replace(startString, ''));
-      }
-    });
-      return ( arr.join("-"));
-  }
+export default function cleanSet(set, startString) {
+  let result = '';
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
+    if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
+}
